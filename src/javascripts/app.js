45588453115a847e86041235f11967d7fc7684f2
Marcel Legane <mlegane@gmail.com>
2017-01-04 23:19:53 +0000

    Add showNextPage function

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,21 @@
-import './modules/module'
+import $ from 'jquery';
+window.$ = $;
 
-console.log(`app.js has loaded!`)
+const $carousel = $(`.carousel`);
+const $pages = $(`.page`);
+const pagesLength = $pages.length;
+const activeClass = `is-active`;
+let pageIndex = 0;
+
+const showNextPage = () => {
+  if (pageIndex === pagesLength - 1) {
+    pageIndex = 0;
+  } else {
+    pageIndex++;
+  }
+
+  $pages.removeClass(activeClass);
+  $pages.eq(pageIndex).addClass(activeClass);
+};
+
+$carousel.on(`click`, showNextPage);
